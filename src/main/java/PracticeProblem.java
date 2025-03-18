fc475de4a1bc3993eab0d4806f1826089111f10f
jonathan-zhao-ics4u1-03-25 <jonathan.zhao@student.tdsb.on.ca>
2025-03-18 18:47:49 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,37 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int find(int[] arr, int num) {
+		for(int i = 0; i < arr.length; i++) {
+			if(num == arr[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
+	public static int findLast(String[] arr, String str) {
+		for(int i = arr.length - 1; i >= 0; i--) {
+			if(arr[i].equals(str)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public static int findSecond(char[] arr, char c){
+		int index = -1;
+		int counter = 0;
+			for (int i =0; i < arr.length; i++){
+				if (arr[i] == c){
+					counter++;
+					index = i;
+		}
+			if (counter ==2){
+				break;
+			}
+		}
+		return index;
+	}	
+	
 }
